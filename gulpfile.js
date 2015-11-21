661ded2dbe364b8cc1838ddf0324b5e59f5e1da7
Danny Libin <omegasol11@gmail.com>
2015-11-20 22:24:22 -0600

    Angular 2 alpha 46, changed file structure for increasing app complexity, added Sass to build process

--- gulpfile.js
+++ gulpfile.js
@@ -2,34 +2,52 @@ var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
 var tsc = require('gulp-typescript');
 var tsProject = tsc.createProject('tsconfig.json');
-var config = require('./gulp.config')();
+var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var jasmine = require('gulp-jasmine');
 
 // Start a local server in base directory after compile-ts runs
-gulp.task('serve', ['compile-ts'], function() {
+gulp.task('serve', ['compile-ts', 'compile-scss'], function() {
 	browserSync.init({
 		server: {
-			baseDir: './'
+			baseDir: './dist'
 		}
 	});
 	
 	// Watch for changes in html and ts files in base directory, reload if they occur
-	gulp.watch(['*.html', '*.css'], browserSync.reload);
-	gulp.watch(['*.ts'], ['ts-watch']);
+	gulp.watch(['./dist/html/*.html', './dist/index.html'], browserSync.reload);
+	gulp.watch(['./src/ts/*.ts', './src/scss/*.scss'], ['src-watch']);
 	
 });
 
 // Make sure the compile-ts task completes before reloading browsers
-gulp.task('ts-watch', ['compile-ts', 'reload']);
+gulp.task('src-watch', ['compile-ts', 'compile-scss', 'reload']);
 
-gulp.task('reload', ['compile-ts'], function() {
+gulp.task('reload', ['compile-ts', 'compile-scss'], function() {
 	browserSync.reload();
+});
+
+gulp.task('compile-scss', function() {
+	var sourceScssFiles = [
+		'./src/scss/*.scss'
+	];
+	
+	var scssResult = gulp
+		.src(sourceScssFiles)
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError));
+		
+	var stream = scssResult
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('./dist/css'));
+		
+	return stream;
 });
 
 gulp.task('compile-ts', function() {
 	var sourceTsFiles = [
-		config.allTs,
-		config.typings
+		'./src/ts/*.ts',			// Path to typscript files
+		'./typings/**/*.ts'			// Reference to typings so tsc knows where it is
 	];
 	
 	var tsResult = gulp
@@ -39,7 +57,18 @@ gulp.task('compile-ts', function() {
 	
 	var stream = tsResult
 		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest(config.tsOutputPath));
+		.pipe(gulp.dest('./dist/js'));
 		
 	return stream;
+});
+
+gulp.task('test', function() {
+	gulp.run('run-test');
+	gulp.watch(['./dist/js/display-service.spec.js'], ['run-test']);
+});
+
+gulp.task('run-test', function() {
+	var filesForTest = ['./dist/js/display-service.spec.js'];
+	return gulp.src(filesForTest)
+		.pipe(jasmine());
 });
